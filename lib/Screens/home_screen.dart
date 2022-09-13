d54229dcbc006fee78ed5d7f3b42135016d69e5d
ugocode <obiohaugo5@gmail.com>
2022-09-13 19:39:04 +0100

    added geo fire function to get drivers movement

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,16 +1,385 @@
+import 'dart:async';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_geofire/flutter_geofire.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class HomePage extends StatelessWidget {
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../Global/global.dart';
+import '../Services/assistant_methods.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  GoogleMapController? newGoogleMapController;
+
+  final Completer<GoogleMapController> _controllerGoogleMap = Completer();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  // static const CameraPosition _kLake = CameraPosition(
+  //     bearing: 192.8334901395799,
+  //     target: LatLng(37.43296265331129, -122.08832357078792),
+  //     tilt: 59.440717697143555,
+  //     zoom: 19.151926040649414);
+
+  Position? driverCurrentPosition;
+  var geoLocator = Geolocator();
+  LocationPermission? _locationPermission;
+
+  //function to check if permission is allowed function
+  checkIfLocationPermissionAllowed() async {
+    _locationPermission = await Geolocator.requestPermission();
+
+    if (_locationPermission == LocationPermission.denied) {
+      _locationPermission = await Geolocator.requestPermission();
+    }
+  }
+
+  locateDriverPosition() async {
+    Position cPosition = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    driverCurrentPosition = cPosition;
+
+    LatLng latLngPosition = LatLng(
+        driverCurrentPosition!.latitude, driverCurrentPosition!.longitude);
+
+//animate the camera around the position:
+    CameraPosition cameraPosition =
+        CameraPosition(target: latLngPosition, zoom: 14);
+
+    newGoogleMapController!
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+    String humanreadableAddress =
+        // ignore: use_build_context_synchronously
+        await AssistantMethods.searchAddressforGeographicCoordinates(
+            driverCurrentPosition!, context);
+
+    print("This is your address:   $humanreadableAddress");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    //get the current user info:
+    AssistantMethods.readCurrentOnlineUserInfo();
+    //call the permission function
+    checkIfLocationPermissionAllowed();
+  }
+
+  String statusText = "Now Offline";
+  Color statusColor = Colors.blueGrey;
+  bool isDriverActive = false;
+
+//for Dark mode:
+  blackThemeGoogleMap() {
+    newGoogleMapController!.setMapStyle('''
+                    [
+                      {
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#242f3e"
+                          }
+                        ]
+                      },
+                      {
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#746855"
+                          }
+                        ]
+                      },
+                      {
+                        "elementType": "labels.text.stroke",
+                        "stylers": [
+                          {
+                            "color": "#242f3e"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "administrative.locality",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#d59563"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "poi",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#d59563"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "poi.park",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#263c3f"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "poi.park",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#6b9a76"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#38414e"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road",
+                        "elementType": "geometry.stroke",
+                        "stylers": [
+                          {
+                            "color": "#212a37"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#9ca5b3"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road.highway",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#746855"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road.highway",
+                        "elementType": "geometry.stroke",
+                        "stylers": [
+                          {
+                            "color": "#1f2835"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road.highway",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#f3d19c"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "transit",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#2f3948"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "transit.station",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#d59563"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "water",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#17263c"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "water",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#515c6d"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "water",
+                        "elementType": "labels.text.stroke",
+                        "stylers": [
+                          {
+                            "color": "#17263c"
+                          }
+                        ]
+                      }
+                    ]
+                ''');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flash⚡️ Ryde '),
-        centerTitle: true,
-      ),
-      body: const Center(child: Text('haloo people')),
-    );
+        // appBar: AppBar(
+        //   title: const Text('Flash⚡️ Ryde '),
+        //   centerTitle: true,
+        //   leading: const Text(""),
+        // ),
+        body: Stack(
+      children: [
+        GoogleMap(
+          mapType: MapType.normal,
+          myLocationEnabled: true,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (controller) {
+            _controllerGoogleMap.complete(controller);
+            newGoogleMapController = controller;
+
+            //black theme google map:
+            blackThemeGoogleMap();
+            locateDriverPosition();
+          },
+        ),
+        //ui for online and off line driver:
+        statusText != "Now Online"
+            ? Container(
+                height: MediaQuery.of(context).size.height,
+                width: double.infinity,
+                color: Colors.black87,
+              )
+            : Container(),
+
+        //button for online and offline
+        Positioned(
+            top: statusText != "Now Online"
+                ? MediaQuery.of(context).size.height * 0.45
+                : 25,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      if (isDriverActive != true) {
+                        driverIsOnlineNow();
+                        updateDriversLocationAtRealtime();
+
+                        setState(() {
+                          statusText = "Now Online";
+                          isDriverActive = true;
+                          // buttonColor
+                        });
+
+                        //display Toast
+                        Fluttertoast.showToast(msg: "You are now Online");
+                      } else {
+                        driverIsOffLineNow();
+                        setState(() {
+                          statusText = "Now Offline";
+                          isDriverActive = false;
+                          // buttonColor
+                        });
+                        Fluttertoast.showToast(msg: "You are now Offline");
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(26))),
+                    child: statusText != "Now Online"
+                        ? Text(
+                            statusText,
+                            style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          )
+                        : const Icon(
+                            Icons.phonelink_ring,
+                            color: Colors.white,
+                            size: 26,
+                          ))
+              ],
+            ))
+      ],
+    ));
+  }
+
+  driverIsOnlineNow() async {
+    Position pos = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    driverCurrentPosition = pos;
+
+    Geofire.initialize("activeDrivers");
+    Geofire.setLocation(currentFirebaseUser!.uid,
+        driverCurrentPosition!.latitude, driverCurrentPosition!.longitude);
+    var newRide =
+        driversRef.child(currentFirebaseUser!.uid).child("newRideStatus");
+
+    newRide.set("idle"); //serching for ride request
+    newRide.onValue.listen((event) {}); //waiting for request
+  }
+
+  updateDriversLocationAtRealtime() {
+    streamSubscriptionPosition =
+        Geolocator.getPositionStream().listen((Position position) {
+      if (isDriverActive == true) {
+        Geofire.setLocation(currentFirebaseUser!.uid,
+            driverCurrentPosition!.latitude, driverCurrentPosition!.longitude);
+      }
+
+      LatLng latLng = LatLng(
+          driverCurrentPosition!.latitude, driverCurrentPosition!.longitude);
+
+      newGoogleMapController!.animateCamera(CameraUpdate.newLatLng(latLng));
+    });
+  }
+
+  driverIsOffLineNow() {
+    Geofire.removeLocation(currentFirebaseUser!.uid);
+
+    DatabaseReference? ref =
+        driversRef.child(currentFirebaseUser!.uid).child("newRideStatus");
+    ref.onDisconnect();
+    ref.remove();
+    ref = null;
+
+    Future.delayed(const Duration(milliseconds: 2000), () {
+      SystemChannels.platform.invokeMethod("SystemNavigator.pop");
+    });
   }
 }
