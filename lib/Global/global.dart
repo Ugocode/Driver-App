55153cdf92d5c0c8dba4c173a1ec5623ddaa9f32
ugocode <obiohaugo5@gmail.com>
2022-10-19 10:15:39 +0100

    modified the global . dart page

--- lib/Global/global.dart
+++ lib/Global/global.dart
@@ -1,8 +1,19 @@
+//import 'package:drivers_app/Models/users_model.dart';
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:geolocator/geolocator.dart';
 
 final FirebaseAuth fireAuth = FirebaseAuth.instance;
 
-final DatabaseReference driversRef = FirebaseDatabase.instance.ref();
+DatabaseReference driversRef = FirebaseDatabase.instance.ref().child("drivers");
+
+final DatabaseReference usersRef =
+    FirebaseDatabase.instance.ref().child("users");
 
 User? currentFirebaseUser;
+//UserModel? userModelCurrentInfo;
+
+//strem substrcription
+StreamSubscription<Position>? streamSubscriptionPosition;
