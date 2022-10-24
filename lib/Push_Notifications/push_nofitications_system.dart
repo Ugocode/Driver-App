536e56aa976c35345c76256cfab5001e72d05181
ugocode <obiohaugo5@gmail.com>
2022-10-24 10:21:56 +0100

    added rideuserRequest method

--- lib/Push_Notifications/push_nofitications_system.dart
+++ lib/Push_Notifications/push_nofitications_system.dart
@@ -1,6 +1,9 @@
 import 'package:drivers_app/Global/global.dart';
+import 'package:drivers_app/Models/user_ride_request_info.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PushNotificationSystem {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
@@ -11,24 +14,88 @@ class PushNotificationSystem {
     FirebaseMessaging.instance
         .getInitialMessage()
         .then((RemoteMessage? remoteMessage) {
-      if (remoteMessage != null) {
-        //deisplay the ride request information = user information who requested the ride
-      }
+      if (remoteMessage != null) {}
     });
 
 /////////////////////////////////////////////////////////////////////////////
     //2. Forground state
     //when the app is opned and in use:
-    FirebaseMessaging.onMessage.listen((RemoteMessage? remoteMessage) {});
+    FirebaseMessaging.onMessage.listen((RemoteMessage? remoteMessage) {
+      print(
+          "this is the remote message => \n ${remoteMessage!.data["rideRequestId"]}");
+
+      //deisplay the ride request information = user information who requested the ride
+      readUserRideRequestInformation(remoteMessage.data["rideRequestId"]);
+    });
 
 /////////////////////////////////////////////////////////////////////////////
     //3. Background state
     //for when the app is in the background;
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage? remoteMessage) {
       //desplay the ride request information = user information who requested the ride
+      print(
+          "this is the remote message => \n ${remoteMessage!.data["rideRequestId"]}");
+
+      //deisplay the ride request information = user information who requested the ride
+      readUserRideRequestInformation(remoteMessage.data["rideRequestId"]);
     });
   }
 
+//reide request function:
+  readUserRideRequestInformation(String userRideRequestId) {
+    FirebaseDatabase.instance
+        .ref()
+        .child("All Ride Requests")
+        .child(userRideRequestId)
+        .once()
+        .then((snapData) {
+      if (snapData.snapshot.value != null) {
+        double originLat = double.parse(
+            (snapData.snapshot.value! as Map)["origin"]["latitude"].toString());
+        double originLng = double.parse(
+            (snapData.snapshot.value! as Map)["origin"]["longitude"]
+                .toString());
+
+        String originAddress =
+            (snapData.snapshot.value! as Map)["originAddress"];
+
+        //for destionation:
+        double destinationLat = double.parse(
+            (snapData.snapshot.value! as Map)["destination"]["latitude"]
+                .toString());
+        double destinationLng = double.parse(
+            (snapData.snapshot.value! as Map)["destination"]["longitude"]
+                .toString());
+
+        String destinationAddress =
+            (snapData.snapshot.value! as Map)["destinationAddress"];
+
+        String userName = (snapData.snapshot.value! as Map)["userName"];
+        String userPhone = (snapData.snapshot.value! as Map)["userPhone"];
+
+        //create an instance to get the above attributes and values:
+        UserRideRequestInformation userRideRequestDetails =
+            UserRideRequestInformation();
+
+        userRideRequestDetails.originLatLng = LatLng(originLat, originLng);
+        userRideRequestDetails.originAddress = originAddress;
+
+        userRideRequestDetails.destinationAddress = destinationAddress;
+        userRideRequestDetails.destinationLatLng =
+            LatLng(destinationLat, destinationLng);
+
+        userRideRequestDetails.userName = userName;
+        userRideRequestDetails.userPhone = userPhone;
+
+        print(
+            "user rsde request info::=> $userName \n $userPhone \n $originAddress");
+      } else {
+        Fluttertoast.showToast(msg: "This RideRequest id do not exist");
+      }
+    });
+  }
+
+  //to generate token:
   Future generateAndGetToken() async {
     //get the registration token and apply:
     String? registrationToken = await messaging.getToken();
