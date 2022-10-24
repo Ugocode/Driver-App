5bdb22d3dfa9dc5306791b87f63934c11cb25db4
ugocode <obiohaugo5@gmail.com>
2022-10-24 10:22:48 +0100

    added the save driveringo method

--- lib/Authentication/registration_screen.dart
+++ lib/Authentication/registration_screen.dart
@@ -1,6 +1,11 @@
-import 'package:drivers_app/Authentication/car_registration.dart';
 import 'package:drivers_app/Authentication/login_screen.dart';
+import 'package:drivers_app/Global/global.dart';
+import 'package:drivers_app/Widgets/progress_dialog.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'car_registration.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({Key? key}) : super(key: key);
@@ -18,6 +23,90 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       TextEditingController();
   final TextEditingController _passwordTextEditingController =
       TextEditingController();
+
+  //to save the driver information to firebase datasbase we:
+  saveDriverInfoNow() async {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: ((context) {
+        return const ProgressDialog(
+          message: "Processing, Please wait...",
+        );
+      }),
+    );
+
+    final User? firebaseUser = (await fireAuth
+            .createUserWithEmailAndPassword(
+      email: _emailTextEditingController.text.trim(),
+      password: _passwordTextEditingController.text.trim(),
+    )
+            .catchError((eMsg) {
+      Navigator.pop(context);
+      //show the error message
+      Fluttertoast.showToast(msg: "⚠️ Error: \n$eMsg");
+    }))
+        .user;
+
+    //to save the credentials to firebase database:
+    if (firebaseUser != null) {
+      //save to firestore
+      Map driversMap = {
+        "driverID": firebaseUser.uid,
+        "diverName": _nameTextEditingController.text.trim(),
+        "driverEmail": _emailTextEditingController.text.trim(),
+        "driverPhone": _phoneTextEditingController.text.trim(),
+      };
+      //get the databse ref:
+      // driversRef.child("drivers");
+      driversRef.child(firebaseUser.uid).set(driversMap);
+
+      currentFirebaseUser = firebaseUser;
+      Fluttertoast.showToast(msg: "Account has been created!");
+      // go to register your car:
+      // ignore: use_build_context_synchronously
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => const CarRegistrationScreen()));
+    } else {
+      // ignore: use_build_context_synchronously
+      Navigator.pop(context);
+      Fluttertoast.showToast(msg: "⚠️ Account has Not been created..");
+    }
+  }
+
+//to validate  our form before saving data:
+  validatForm() {
+    if (_nameTextEditingController.text.length < 3) {
+      Fluttertoast.showToast(
+          msg: "⚠️ Name must be at least 3 characters long",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.TOP,
+          backgroundColor: Colors.red);
+    } else if (!_emailTextEditingController.text.contains("@")) {
+      Fluttertoast.showToast(
+          msg: "⚠️ Email is badly formatted",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.TOP,
+          backgroundColor: Colors.red);
+    } else if (_passwordTextEditingController.text.length < 5) {
+      Fluttertoast.showToast(
+          msg: "⚠️ Password is too short",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.TOP,
+          backgroundColor: Colors.red);
+    } else if (_phoneTextEditingController.text.isEmpty) {
+      Fluttertoast.showToast(
+          msg: "⚠️ Phone number is required",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.TOP,
+          backgroundColor: Colors.red);
+    } else {
+      //save the data to firesore
+      saveDriverInfoNow();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -90,12 +179,13 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             ),
             ElevatedButton(
               onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const CarRegistrationScreen(),
-                  ),
-                );
+                validatForm();
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) => const CarRegistrationScreen(),
+                //   ),
+                // );
               },
               child: const Text(
                 'Create Account',
