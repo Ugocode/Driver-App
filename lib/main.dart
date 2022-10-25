17991b2a29424cd2dc132d99906b2db2d77a045f
ugocode <obiohaugo5@gmail.com>
2022-10-25 17:24:22 +0100

    added change notfier provider to materialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
+import 'package:drivers_app/Global/info_handler.dart';
 import 'package:drivers_app/Screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MyApp(
-        child: MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Flash Driver',
-            theme: ThemeData(primarySwatch: Colors.pink),
-            home: const MySplashScreen())),
+        child: ChangeNotifierProvider(
+      create: (context) => AppInfo(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flash Driver',
+          theme: ThemeData(primarySwatch: Colors.pink),
+          home: const MySplashScreen()),
+    )),
   );
 }
 
